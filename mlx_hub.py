5ed8a1ceb19fd5b0a7de3db4dbc6d9989fb26d70
Gaurav Aggarwal <grwl888@gmail.com>
2024-07-16 15:33:31 -0400

    * Added unit tests for mlx-hub  * Removed unnecessary conversion to list in mlx_hub  * Simplified list comprehensions in suggest and scan functions  * Improved return logic in download function  * Consistent use of return statements in error cases

--- mlx_hub.py
+++ mlx_hub.py
@@ -1,11 +1,8 @@
+from enum import Enum
 from typing import List
 from huggingface_hub import HfApi, scan_cache_dir, snapshot_download
-from enum import Enum
-
-from huggingface_hub.hf_api import ModelInfo
 
 SUGGESTED_MODELS_FILE_PATH = 'suggested_models.txt'
-
 SEARCH_AUTHOR = "mlx-community"
 SEARCH_LIBRARY = 'mlx'
 SEARCH_FULL = False
@@ -22,56 +19,51 @@ class SortBy(Enum):
 def search(search_term, search_limit=SEARCH_LIMIT, sort_by=SortBy.DOWNLOADS) -> List[str]:
     """Searches for model repositories using a string that contain complete or partial names for models on the Hub."""
     hf_api = HfApi()
-    # List all models with the specified filter
     models = hf_api.list_models(
         search=search_term,
         library=SEARCH_LIBRARY,
+        author=SEARCH_AUTHOR,
         sort=sort_by.value,
         full=SEARCH_FULL,
         limit=search_limit
     )
-    models_list = list(models)
-    return [model.id for model in models_list]
+    return [model.id for model in models]
 
 
-def suggest():
+def suggest() -> List[str]:
     """Reads and returns suggested models from a file."""
     try:
         with open(SUGGESTED_MODELS_FILE_PATH, 'r') as file:
             lines = file.readlines()
-            # Strip newline characters from each line
-            lines = [line.strip() for line in lines]
-        return lines
+            return [line.strip() for line in lines]
     except (FileNotFoundError, IOError):
         print(f"An error occurred while reading the file at path {SUGGESTED_MODELS_FILE_PATH}.")
-    return []
+        return []
 
 
-def scan():
+def scan() -> List[str]:
     """Scans the Hugging Face cache directory and returns a list of repositories."""
     hf_cache_info = scan_cache_dir()
-    models_list = list(hf_cache_info.repos)
-    return [model.repo_id for model in models_list]
+    return [model.repo_id for model in hf_cache_info.repos]
 
 
-def download(repo_id):
+def download(repo_id: str) -> bool:
     """Downloads a repository snapshot by its ID."""
     try:
         repo_path = snapshot_download(repo_id)
-        return True if repo_path else False
+        return bool(repo_path)
     except Exception as e:
         print(f"An error occurred while downloading the repository {repo_id}: {e}")
         return False
 
 
-def delete(repo_id):
+def delete(repo_id: str) -> bool:
     """Deletes a repository by its ID."""
     hf_cache_info = scan_cache_dir()
     for repo in hf_cache_info.repos:
         if repo_id == repo.repo_id:
             try:
-                hf_cache_info = scan_cache_dir()
-                for revision in sorted(repo.revisions, key=lambda revision_hash: revision_hash.commit_hash):
+                for revision in sorted(repo.revisions, key=lambda revision: revision.commit_hash):
                     strategy = hf_cache_info.delete_revisions(revision.commit_hash)
                     strategy.execute()
                 return True
---
+++ test_mlx_hub.py
@@ -0,0 +1,96 @@
+import unittest
+import mlx_hub
+
+TEST_MODEL = "mlx-community/TinyDolphin-2.8-1.1b-4bit-mlx"
+TEST_SEARCH_PHRASE_MANY_RESULTS = "llama"
+TEST_SEARCH_PHRASE_FEW_RESULTS = "bert"
+TEST_SEARCH_PHRASE_NO_RESULTS = "bad_search_phrase"
+TEST_SEARCH_PHRASE_EMPTY = ""
+
+
+class TestMLXHub(unittest.TestCase):
+    def test_search(self):
+        result = mlx_hub.search(TEST_SEARCH_PHRASE_MANY_RESULTS)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), mlx_hub.SEARCH_LIMIT)
+
+        for repo_id in result:
+            self.assertIsNotNone(repo_id)
+            self.assertGreater(len(repo_id), 0)
+            self.assertEqual(len(repo_id.split("/")), 2)
+
+        result = mlx_hub.search(TEST_SEARCH_PHRASE_FEW_RESULTS)
+        self.assertIsNotNone(result)
+        self.assertLess(len(result), mlx_hub.SEARCH_LIMIT)
+
+        for repo_id in result:
+            self.assertIsNotNone(repo_id)
+            self.assertGreater(len(repo_id), 0)
+            self.assertEqual(len(repo_id.split("/")), 2)
+
+        result = mlx_hub.search(TEST_SEARCH_PHRASE_NO_RESULTS)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 0)
+
+        result = mlx_hub.search(TEST_SEARCH_PHRASE_EMPTY)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), mlx_hub.SEARCH_LIMIT)
+
+    def test_suggest(self):
+        result = mlx_hub.suggest()
+        self.assertIsNotNone(result)
+        self.assertGreaterEqual(len(result), 0)
+
+        for repo_id in result:
+            self.assertIsNotNone(repo_id)
+            self.assertGreater(len(repo_id), 0)
+            self.assertEqual(len(repo_id.split("/")), 2)
+
+    def test_scan(self):
+        result = mlx_hub.scan()
+        self.assertIsNotNone(result)
+        self.assertGreaterEqual(len(result), 0)
+
+        if TEST_MODEL not in result:
+            download_result = mlx_hub.download(TEST_MODEL)
+            self.assertEqual(download_result, True)
+            result = mlx_hub.scan()
+            self.assertIn(TEST_MODEL, result)
+
+        for repo_id in result:
+            self.assertIsNotNone(repo_id)
+            self.assertGreater(len(repo_id), 0)
+            self.assertEqual(len(repo_id.split("/")), 2)
+
+    def test_download(self):
+        scan_result_before_download = mlx_hub.scan()
+
+        if TEST_MODEL in scan_result_before_download:
+            delete_result = mlx_hub.delete(TEST_MODEL)
+            self.assertEqual(delete_result, True)
+            self.assertNotIn(TEST_MODEL, mlx_hub.scan())
+
+        download_result = mlx_hub.download(TEST_MODEL)
+        self.assertTrue(download_result)
+
+        scan_result_after = mlx_hub.scan()
+        self.assertIn(TEST_MODEL, scan_result_after)
+
+    def test_delete(self):
+        scan_results_before_delete = mlx_hub.scan()
+
+        if TEST_MODEL not in scan_results_before_delete:
+            download_result = mlx_hub.download(TEST_MODEL)
+            self.assertEqual(download_result, True)
+            scan_results_after_download = mlx_hub.scan()
+            self.assertIn(TEST_MODEL, scan_results_after_download)
+
+        delete_result = mlx_hub.delete(TEST_MODEL)
+        self.assertEqual(delete_result, True)
+
+        scan_results_after_delete = mlx_hub.scan()
+        self.assertNotIn(TEST_MODEL, scan_results_after_delete)
+
+
+if __name__ == '__main__':
+    unittest.main()
