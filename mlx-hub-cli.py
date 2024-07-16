7f47d47f4f6acb3e9b66a0a81e992f5fc5b420fc
Gaurav Aggarwal <grwl888@gmail.com>
2024-07-16 03:12:58 -0400

    * Updated the model list  * Moved to PyCharm  * Reformated code

--- mlx-hub-cli.py
+++ mlx-hub-cli.py
@@ -3,84 +3,97 @@ import mlx_hub
 from enum import Enum
 from cli_utils import print_string_list, print_repo_list, print_model_list
 
+
 class Action(Enum):
-    HELP = ("help", "Show this help message")
-    START = ("start", "Start interactive mode", False)
-    SCAN = ("scan", "Scan for downloaded MLX models")
-    SEARCH = ("search", "Search for MLX models using a search term")
-    SUGGEST = ("suggest", "Suggest MLX models to download")
-    DOWNLOAD = ("download", "Download a specific model")
-    DELETE = ("delete", "Delete a specific model")
-    EXIT = ("exit", "Exit the application")
-    INVALID = ("invalid", "Invalid action", False)
-    
-    def __new__(cls, value, description, is_interactive=True):
+    HELP = ("help", "", "Show this help message")
+    START = ("start", "", "Start Interactive Mode", False)
+    SCAN = ("scan", "", "Scan for downloaded MLX models")
+    SEARCH = ("search", "phrase", "Search for MLX models using a search phrase")
+    SUGGEST = ("suggest", "", "Suggest MLX models to download")
+    DOWNLOAD = ("download", "repo_id", "Download a specific model")
+    DELETE = ("delete", "repo_id", "Delete a specific model")
+    EXIT = ("exit", "", "Exit Interactive Mode")
+    INVALID = ("invalid", "", "Invalid action", False)
+
+    def __new__(cls, value: str, parameter: str, description: str, is_interactive: bool = True):
         obj = object.__new__(cls)
         obj._value_ = value
         obj.option = "--" + value
+        obj.parameter = parameter
         obj.description = description
         obj.interactive = is_interactive
         return obj
 
+
 def print_action_help():
-    """Prints available actions for the interactive mode."""
+    """Prints available actions for interactive mode."""
     print("Available Actions:")
     for action in Action:
         if action.interactive:
-            print(f"    {action.value:<15} {action.description}")
+            print(f"    {action.value:<8} {action.parameter:<12} {action.description}")
 
-def execute_action(action, args=None):
-    """Executes the given action with optional arguments."""
+
+def execute_action(action, parameters=None):
+    """Executes the given action with optional parameters."""
     print()
-    
-    if action == Action.SCAN:
+
+    if action == Action.HELP:
+        print_action_help()
+
+    elif action == Action.SCAN:
         repo_list = mlx_hub.scan()
-        print(f"Downloaded models: {len(repo_list)}")
+        if len(repo_list) == 0:
+            print("No downloaded models found.")
+        else:
+            print(f"{len(repo_list)} downloaded models: ")
         print_repo_list(repo_list)
-    
+
     elif action == Action.SEARCH:
-        if args:
-            models_list = mlx_hub.search(args)
-            print(f"Models found: {len(models_list)}")
+        if parameters:
+            models_list = mlx_hub.search(parameters)
+            count = len(models_list)
+            if count == 0:
+                print("No models found.")
+            elif count == mlx_hub.SEARCH_LIMIT:
+                print(f"Showing top {mlx_hub.SEARCH_LIMIT} results:")
+            else:
+                print(f"{count} models found:")
             print_model_list(models_list)
         else:
-            print("Please provide a search term.")
-    
+            print("Please provide a search phrase.")
+
     elif action == Action.SUGGEST:
         print("Suggested models:")
         print_string_list(mlx_hub.suggest())
-                  
+
     elif action == Action.DOWNLOAD:
-        if args:
-            print(f"Downloading model: {args}")
-            if mlx_hub.download(args):
+        if parameters:
+            print(f"Downloading model: {parameters}")
+            if mlx_hub.download(parameters):
                 print("Model downloaded successfully.")
             else:
                 print("Model not found.")
         else:
             print("Please provide a Repo ID.")
-            
+
     elif action == Action.DELETE:
-        if args:
-            print(f"Deleting model: {args}")
-            if mlx_hub.delete(args):
+        if parameters:
+            print(f"Deleting model: {parameters}")
+            if mlx_hub.delete(parameters):
                 print("Model deleted successfully.")
             else:
                 print("Model not found.")
         else:
             print("Please provide a Repo ID.")
-    
+
     elif action == Action.START:
         print("Starting interactive mode.")
         execute_action(Action.HELP)
         start_interactive_mode()
-        
+
     elif action == Action.EXIT:
         print("Goodbye!")
         return False
-    
-    elif action == Action.HELP:
-        print_action_help()
 
     else:
         print("Invalid action.")
@@ -89,38 +102,68 @@ def execute_action(action, args=None):
     print()
     return True
 
+
 def start_interactive_mode():
     """Starts the interactive mode, prompting the user for actions."""
+
     running = True
     while running:
         # Get user input
         user_input = input("Enter Action > ").strip().split()
         if not user_input:
             continue
-        
+
         # Validate action
         try:
             action = Action(user_input[0])
             if not action.interactive:
                 action = Action.INVALID
-            args = user_input[1] if len(user_input) > 1 else None
-                   
+
         except ValueError:
             action = Action.INVALID
 
         # Execute action
-        running = execute_action(action, args)
+        parameter = user_input[1] if len(user_input) > 1 else None
+        running = execute_action(action, parameter)
+
 
 def main():
     """Main function to parse arguments and execute corresponding actions."""
     parser = argparse.ArgumentParser(description='MLX-Hub CLI')
-    parser.add_argument(Action.START.option, action='store_true', help=Action.START.description)
-    parser.add_argument(Action.SCAN.option, action='store_true', help=Action.SCAN.description)
-    parser.add_argument(Action.SEARCH.option, type=str, metavar='search_term', help=Action.SEARCH.description)
-    parser.add_argument(Action.SUGGEST.option, action='store_true', help=Action.SUGGEST.description)
-    parser.add_argument(Action.DOWNLOAD.option, type=str, metavar='model_id', help=Action.DOWNLOAD.description)
-    parser.add_argument(Action.DELETE.option, type=str, metavar='model_id', help=Action.DELETE.description)
-    
+    parser.add_argument(
+        Action.START.option,
+        action='store_true',
+        help=Action.START.description
+    )
+    parser.add_argument(
+        Action.SCAN.option,
+        action='store_true',
+        help=Action.SCAN.description
+    )
+    parser.add_argument(
+        Action.SEARCH.option,
+        type=str,
+        metavar=Action.SEARCH.parameter,
+        help=Action.SEARCH.description
+    )
+    parser.add_argument(
+        Action.SUGGEST.option,
+        action='store_true',
+        help=Action.SUGGEST.description
+    )
+    parser.add_argument(
+        Action.DOWNLOAD.option,
+        type=str,
+        metavar=Action.DOWNLOAD.parameter,
+        help=Action.DOWNLOAD.description
+    )
+    parser.add_argument(
+        Action.DELETE.option,
+        type=str,
+        metavar=Action.DELETE.parameter,
+        help=Action.DELETE.description
+    )
+
     args = parser.parse_args()
 
     try:
@@ -140,6 +183,7 @@ def main():
             execute_action(Action.HELP)
     except Exception as e:
         print(f"An error occurred: {e}")
+
 
 if __name__ == "__main__":
     main()
--- mlx_hub.py
+++ mlx_hub.py
@@ -5,9 +5,9 @@ SUGGESTED_MODELS_FILE_PATH = 'suggested_models.txt'
 
 SEARCH_AUTHOR = "mlx-community"
 SEARCH_LIBRARY = 'mlx'
-SEARCH_TASK = "text-generation" # Unused
 SEARCH_FULL = False
 SEARCH_LIMIT = 25
+
 
 class SortBy(Enum):
     LIKES = "likes"
@@ -15,21 +15,20 @@ class SortBy(Enum):
     CREATED = "created"
     LAST_MODIFIED = "last_modified"
 
+
 def search(search_term, search_limit=SEARCH_LIMIT, sort_by=SortBy.DOWNLOADS):
     """Searches for model repositories using a string that contain complete or partial names for models on the Hub."""
     hf_api = HfApi()
-    
     # List all models with the specified filter
     models = hf_api.list_models(
-        search=search_term, 
-        library=SEARCH_LIBRARY, 
-        author=SEARCH_AUTHOR,
+        search=search_term,
+        library=SEARCH_LIBRARY,
         sort=sort_by.value,
         full=SEARCH_FULL,
         limit=search_limit
     )
-
     return list(models)
+
 
 def suggest():
     """Reads and returns suggested models from a file."""
@@ -43,18 +42,21 @@ def suggest():
         print(f"An error occurred while reading the file at path {SUGGESTED_MODELS_FILE_PATH}.")
     return []
 
+
 def scan():
     """Scans the Hugging Face cache directory and returns a list of repositories."""
     hf_cache_info = scan_cache_dir()
     return hf_cache_info.repos
-        
+
+
 def find(repo_id):
     """Finds and returns a repository by its ID."""
     hf_cache_info = scan_cache_dir()
-    for repo in sorted(hf_cache_info.repos, key=lambda repo: repo.repo_path):
+    for repo in hf_cache_info.repos:
         if repo_id == repo.repo_id:
             return repo
     return None
+
 
 def download(repo_id):
     """Downloads a repository snapshot by its ID."""
@@ -65,13 +67,14 @@ def download(repo_id):
         print(f"An error occurred while downloading the repository {repo_id}: {e}")
         return False
 
+
 def delete(repo_id):
     """Deletes a repository by its ID."""
     repo = find(repo_id)
     if repo is not None:
         try:
             hf_cache_info = scan_cache_dir()
-            for revision in sorted(repo.revisions, key=lambda revision: revision.commit_hash):
+            for revision in sorted(repo.revisions, key=lambda revision_hash: revision_hash.commit_hash):
                 strategy = hf_cache_info.delete_revisions(revision.commit_hash)
                 strategy.execute()
             return True
